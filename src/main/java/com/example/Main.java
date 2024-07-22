d63af1be2159a7a72b4ff0135bfb13d36b2a6209
ivan <belyakov_ivan96@mail.ru>
2024-07-22 08:56:48 +0300

    init

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import com.example.geometry.Circle;
+import com.example.geometry.Rectangle;
+import com.example.geometry.Triangle;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(3, 4, 5);
+
+        System.out.println("Circle Area: " + circle.getArea());
+        System.out.println("Circle Perimeter: " + circle.getPerimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+
+        System.out.println("Triangle Area: " + triangle.getArea());
+        System.out.println("Triangle Perimeter: " + triangle.getPerimeter());
+    }
+}
